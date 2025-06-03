a20bb69294f3ad98cf9d8ea22211697e29b4ad41
SoledadLepio <solelepio@gmail.com>
2025-06-03 11:09:23 -0400

    commit final (este si)
    03-06-2025

--- assets/js/script3.js
+++ assets/js/script3.js
@@ -1,2 +1,26 @@
 //telefono debe tener exactamente 8 digitos, no es obligatoria
 //contrasenia debe tener mas de 5 caracteres, es obligatoria
+
+personas = []
+
+nTelefono= document.getElementById("telefono")
+vTelefono= vTelefono.value
+eTelefono=
+
+
+nContraseña= document.getElementById("contraseña")
+vContraseña= vContraseña.value
+eContraseña= ""
+
+valide= true
+
+
+if{
+    "eTelefono > 9" 
+    vTelefono= 
+    <button button>
+
+
+}
+
+<button button="Telefono" >Telefono</button>
